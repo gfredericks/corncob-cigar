f9722678eac177867e501860ebf6ebaebcbcfc97
Gary Fredericks <fredericksgary@gmail.com>
2014-10-18 12:35:22 -0500

    Initial version of gsub-version

---
+++ src/leiningen/gsub_version.clj
@@ -0,0 +1,21 @@
+(ns leiningen.gsub-version
+  (:require [clojure.string :as s])
+  (:import java.util.regex.Pattern))
+
+(defn gsub-version
+  [project]
+  (let [full-project-name (if (= (:name project) (:group project))
+                            (format "(?:%s/)?%s"
+                                    (Pattern/quote (:group project))
+                                    (Pattern/quote (:name project)))
+                            (format "%s/%s"
+                                    (Pattern/quote (:group project))
+                                    (Pattern/quote (:name project))))
+        version-regex
+        (re-pattern (format "(\\[%s \")[^\\s\\]]+(\"\\])" full-project-name))]
+    (doseq [f (:gsub-version/files project)]
+      (-> (slurp f)
+          (s/replace version-regex
+                     (fn [[_ before after]]
+                       (str before (:version project) after)))
+          (->> (spit f))))))
