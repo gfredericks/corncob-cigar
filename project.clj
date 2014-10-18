e7d1e3d8cd14efbc1c83c04a120dc5423b9c8bf9
Gary Fredericks <fredericksgary@gmail.com>
2014-10-18 13:00:40 -0500

    Set clojars as the deploy repo

--- project.clj
+++ project.clj
@@ -3,4 +3,5 @@
   :url "https://github.com/gfredericks/corncob-cigar"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :eval-in-leiningen true)
+  :eval-in-leiningen true
+  :deploy-repositories [["releases" :clojars]])
