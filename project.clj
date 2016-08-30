edf3c06cf3c6f6f926874fe8209598e68a277b2f
Gary Fredericks <fredericksgary@gmail.com>
2016-08-29 22:19:09 -0500

    Version 0.1.7

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.gfredericks/corncob-cigar "0.1.7-SNAPSHOT"
+(defproject com.gfredericks/corncob-cigar "0.1.7"
   :description "Leiningen utilities."
   :url "https://github.com/gfredericks/corncob-cigar"
   :license {:name "Eclipse Public License"
